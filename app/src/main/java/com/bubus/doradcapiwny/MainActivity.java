da828b8d41929b0974cad63b557f33c0765412f2
mzdrenka <zdrenka.mateusz@gmail.com>
2019-01-15 01:06:18 +0100

    Initial commit

---
+++ app/src/main/java/com/bubus/doradcapiwny/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bubus.doradcapiwny;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
